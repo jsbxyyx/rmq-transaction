80b5ad9130d1619dae490da1afcf252aa24ed6be
jsbxyyx <jsbxyyx@163.com>
2023-09-22 19:25:03 +0800

    conditional on missing bean

--- src/main/java/com/github/jsbxyyx/transaction/rmq/RMQTransactionConfiguration.java
+++ src/main/java/com/github/jsbxyyx/transaction/rmq/RMQTransactionConfiguration.java
@@ -1,5 +1,6 @@
 package com.github.jsbxyyx.transaction.rmq;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -7,16 +8,22 @@ import org.springframework.core.annotation.Order;
 
 import com.github.jsbxyyx.transaction.rmq.schedule.MqMsgSchedule;
 
+/**
+ * @author jsbxyyx
+ * @since 1.0.0
+ */
 @Configuration
 public class RMQTransactionConfiguration {
     
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
+    @ConditionalOnMissingBean
     public SpringContextUtils springContextUtils() {
         return new SpringContextUtils();
     }
     
     @Bean
+    @ConditionalOnMissingBean
     public MqMsgSchedule mqMsgSchedule() {
         return new MqMsgSchedule();
     }
